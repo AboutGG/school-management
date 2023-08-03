3fa974c51ec7088e4c0754eb6afb3b0b8ec0f888
SirJoH <sergiomusumeci@live.it>
2023-08-03 17:11:58 +0200

    added userDto

---
+++ backend/DTO/UserDTO.cs
@@ -0,0 +1,15 @@
+﻿using backend.Utils;
+
+namespace backend.Dto;
+
+public class UserDto
+{
+    /// <summary> User username </summary>
+    [StringValidator(3, ErrorMessage = "Username cannot contain less then 3 character")]
+    public string Username { get; set; }
+
+    /// <summary> User password </summary>
+    [StringValidator(3, ErrorMessage = "Password cannot contain less then 3 character")]
+    public string Password { get; set; }
+}
+
