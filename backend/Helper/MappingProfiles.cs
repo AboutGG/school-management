42b4e3ec998053afec8934b494d640499d2d1a99
AboutGG <gabrielegiuliano3102@gmail.com>
2023-08-03 15:18:33 +0200

    Added Login with jwt verification

---
+++ backend/Helper/MappingProfiles.cs
@@ -0,0 +1,13 @@
+﻿using AutoMapper;
+using backend.DTO;
+using backend.Models;
+
+namespace backend.Helper;
+
+public class MappingProfiles : Profile
+{
+    public MappingProfiles()
+    {
+        CreateMap<User, UserDTO>();
+    }
+}
