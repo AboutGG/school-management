125af84d969fb8235e6bdd22a62ca5a452582b99
angelbj23 <lombardo.angelo11@gmail.com>
2023-08-04 14:16:03 +0200

    creation classes component and adding font icons

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassesComponent } from './components/pages/classes/classes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', title: 'ManageClasses', component: ClassesComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
