6c796495ce3aa83c310fc04d509de157c623ebbb
frasc93 <scollofrancesca93@gmail.com>
2023-08-08 10:38:37 +0200

    feat creation classes and show class components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ClassesComponent } from './components/pages/classes/classes.component';
+import { ShowClassComponent } from './components/pages/classes/show-class/show-class.component';
 
 const routes: Routes = [
+  { path: '', component: ClassesComponent, title: 'Classes'},
+  { path: 'classes', component: ShowClassComponent, title: 'Show Class'}
  
 ];
 
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/layout/navbar/navbar.component';
 import { SidebarComponent } from './components/layout/sidebar/sidebar.component';
+import { ClassesComponent } from './components/pages/classes/classes.component';
+import { ShowClassComponent } from './components/pages/classes/show-class/show-class.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SidebarComponent,
+    ClassesComponent,
+    ShowClassComponent,
   ],
   imports: [
     BrowserModule,
