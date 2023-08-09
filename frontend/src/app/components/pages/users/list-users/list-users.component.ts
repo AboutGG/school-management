8f0942c9f7805438de040b89fa278d3f877f7884
giop5 <giordanapistorio96@gmail.com>
2023-08-09 14:31:50 +0200

    modifiche dati

--- frontend/src/app/components/pages/users/list-users/list-users.component.ts
+++ frontend/src/app/components/pages/users/list-users/list-users.component.ts
@@ -8,10 +8,57 @@ import { Registry, Users } from 'src/app/shared/models/users';
 })
 export class ListUsersComponent {
   users: Registry[] = [{
-    name: "Giordana",
-    surname: "Pistorio",
+    name: "Giorgio",
+    surname: "Catania",
+    gender: "Uomo",
+    birth: "26-03-1991"
+  },{
+    name: "Andrea",
+    surname: "Palermo",
+    gender: "Uomo",
+    birth: "03-12-2001"
+  },
+  {
+    name: "Gigi",
+    surname: "Sedia",
+    gender: "Uomo",
+    birth: "12-05-1986"
+  },
+  {
+    name: "Angela",
+    surname: "Bianco",
     gender: "Femmina",
-    birth: "15-09-1996"
+    birth: "07-02-1995"
+  },
+  {
+    name: "Anna",
+    surname: "Lina",
+    gender: "Other",
+    birth: "20-08-1991"
+  },
+  {
+    name: "Salvo",
+    surname: "Stanco",
+    gender: "Uomo",
+    birth: "13-10-1999"
+  },
+  {
+    name: "Maria",
+    surname: "Quoto",
+    gender: "Femmina",
+    birth: "02-11-1961"
+  },
+  {
+    name: "Mia",
+    surname: "Amica",
+    gender: "Femmina",
+    birth: "24-09-1971"
+  },
+  {
+    name: "Tino",
+    surname: "Gaetano",
+    gender: "Other",
+    birth: "14-12-2003"
   }];
 
   role: string = "";
