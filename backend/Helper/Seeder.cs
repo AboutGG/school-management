c8454d59791bb838941428c90aea4a6a8828af23
AboutGG <gabrielegiuliano3102@gmail.com>
2023-09-01 14:26:31 +0200

    Added classroom entity in Seeder.cs

--- backend/Helper/Seeder.cs
+++ backend/Helper/Seeder.cs
@@ -2,7 +2,7 @@
 using backend.Models;
 using Microsoft.EntityFrameworkCore;
 
-public static class MyDataSeeder
+public static class Seeder
 {
     public static void SeedData(ModelBuilder modelBuilder)
     {
@@ -12,7 +12,16 @@ public static class MyDataSeeder
         );
 
         modelBuilder.Entity<Registry>().HasData(
-            new Registry() {Id = Guid.Parse("d7f23f33-ebf2-4716-8c3f-b997ba2da125"), Name = "Giordana", Surname = "Pistorio", Gender = "Vipera", Birth = DateOnly.Parse("15/09/1996")}
+            new Registry() {Id = Guid.Parse("d7f23f33-ebf2-4716-8c3f-b997ba2da125"), Name = "Giordana", Surname = "Pistorio", Gender = "Vipera", Birth = DateOnly.Parse("15/09/1996")},
+            new Registry() {Id = Guid.Parse("153afc1d-f63f-45aa-ae55-534d4ceeb737"), Name = "Gabriele", Surname = "Giuliano", Gender = "Sirenetta", Birth = DateOnly.Parse("03/01/2002")}
         );
+
+        modelBuilder.Entity<Classroom>().HasData(
+            new Classroom() {Id = Guid.Parse("612ce7d2-c15f-4dca-ac34-676e93f6bb0e"), Name = "1A"},
+            new Classroom() {Id = Guid.Parse("0ed3811a-0a5c-4ed0-b7db-53090199aa27"), Name = "1B"},
+            new Classroom() {Id = Guid.Parse("70f432dc-2a6c-499b-9326-52d1506befa5"), Name = "2A"}
+        );
+        
+        
     }
 }
