c7dcadad741e46a660a95cacb32584d709a0f508
Sidectrl <limonellifrancesco@gmail.com>
2023-10-23 11:47:00 +0200

    Fix: Add the classroom on the exam detail

--- backend/Dto/ExamDto.cs
+++ backend/Dto/ExamDto.cs
@@ -8,6 +8,7 @@ public class ExamDto
     {
         ExamDate = exam.Date;
         Subject = exam.TeacherSubjectClassroom.Subject.Name;
+        Classroom = exam.TeacherSubjectClassroom.Classroom.Name;
         StudentExams = exam.StudentExams.Select(el => new TeacherStudentExamDto()
         {
             Student = new StudentDto(el.Student),
@@ -16,5 +17,6 @@ public class ExamDto
     }
     public DateOnly ExamDate { get; set; }
     public string Subject { get; set; }
+    public string Classroom { get; set; }
     public List<TeacherStudentExamDto> StudentExams { get; set; }
 }
