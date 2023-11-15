f8c5e1c50303223c5a28dd5e584d64833d9df560
federicoAAA <federico.agnello@stevejobs.academy>
2023-11-15 16:02:33 +0100

    "Final login button fixes"

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -16,6 +16,8 @@ export class LoginComponent implements OnInit {
   showInvalidInput: boolean = false;
   error: boolean = false;
   passwordInputType: string = 'password';
+  buttonClicked: boolean = false;
+
 
   ngOnInit(): void {
     this.loginForm = new FormGroup({
@@ -25,12 +27,13 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    this.error = false
+    this.error = false;
+    this.buttonClicked = true;
     this.authService.login(this.loginForm.value).subscribe((res: any) => {
       localStorage.setItem('token', res.token);
       localStorage.setItem('role', res.role);
-      console.log("Sono res di login.ts", res);
-      console.log("Sono token di login.ts " + res.token);
+      console.log("res di login.ts", res);
+      console.log("token di login.ts " + res.token);
       this.router.navigate(['']);
     }, () => {
       this.error = true;
