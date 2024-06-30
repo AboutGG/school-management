0bd28ee8a5b197fe5a22a1651b0fe526a12531d1
giop5 <giordanapistorio96@gmail.com>
2024-06-30 10:01:56 +0200

    aggiunta subscribe al componente classes e show-class

--- frontend/src/app/components/pages/classes/show-class/show-class.component.ts
+++ frontend/src/app/components/pages/classes/show-class/show-class.component.ts
@@ -8,6 +8,7 @@ import { ListResponse } from 'src/app/shared/models/listresponse';
 import { HttpParams } from '@angular/common/http';
 import { Form, FormControl, FormGroup, Validators} from "@angular/forms";
 import { Grade, StudentGraduation } from 'src/app/shared/models/studentgraduation';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: "app-show-class",
@@ -17,7 +18,7 @@ import { Grade, StudentGraduation } from 'src/app/shared/models/studentgraduatio
 export class ShowClassComponent {
   classId!: string;
   classDetails!: ClassDetails;
-  isTeacher: boolean = false; //memorizza se l'utente è insegnante
+  isTeacher: boolean = false;
   order: string = "Registry.Surname";
 
   userGraduation!: StudentGraduation;
@@ -30,12 +31,11 @@ export class ShowClassComponent {
   finalGrade!: number;
   nameSurname!: string;
   promotion: boolean = true;
-  // historyPromotionForm!: FormGroup;
   currentDate = new Date();
   promotionStartDate = new Date();
   promotionEndDate = new Date();
 
-
+  unsubscribe$: Subject<boolean> = new Subject<boolean>();
 
   constructor(
     private classroomService: ClassroomService,
@@ -50,7 +50,6 @@ export class ShowClassComponent {
     });
     this.fetchClassDetails();
     this.isTeacher = this.authService.isTeacher();
-    console.log();
 
     this.graduationForm = new FormGroup({
       scholasticBehavior: new FormControl(null, Validators.required),
@@ -58,48 +57,44 @@ export class ShowClassComponent {
       nextClassroom: new FormControl(null, Validators.required),
     });
 
-    // this.historyPromotionForm = new FormGroup({
-    //   studentId: new FormControl(null),
-    // });
-
     this.promotionStartDate.setMonth(5);
     this.promotionStartDate.setDate(1);
-
     this.promotionEndDate.setMonth(6);
-    this.promotionEndDate.setDate(31);  
+    this.promotionEndDate.setDate(31);
 
     this.getClassroom();
   }
 
   fetchClassDetails() {
     const params = new HttpParams().set("Order", this.order);
-    this.classroomService.getSingleClassroom(this.classId, params).subscribe({
-      next: (res: ListResponse<ClassDetails>) => {
-        this.classDetails = res.data;
-        console.log(this.classDetails.students);
-        
-        this.classDetails.students.map((student: Students) => {
-          const userId = student.id;
-          this.classroomService.getGrade(this.classId, userId!).subscribe({
-            next: (res) => {
-              student.finalGrade = res.finalGrade;
-              student.fullName = res.fullName;
-              // this.finalGrade = student.finalGrade;
-              console.log(student.finalGrade);
-              console.log(student.fullName);
-            },
-          });
+    this.classroomService
+      .getSingleClassroom(this.classId, params)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: (res: ListResponse<ClassDetails>) => {
+          this.classDetails = res.data;
+          console.log(this.classDetails.students);
 
-          // console.log(student.id);
-          // const dummy  = this.getFinalGrade(student.id!);
-          // student.finalGrade = dummy.
-        });
-        console.log(res.data);
-      },
-      error: (err) => {
-        console.log("error", err);
-      },
-    });
+          this.classDetails.students.map((student: Students) => {
+            const userId = student.id;
+            this.classroomService
+              .getGrade(this.classId, userId!)
+              .pipe(takeUntil(this.unsubscribe$))
+              .subscribe({
+                next: (res) => {
+                  student.finalGrade = res.finalGrade;
+                  student.fullName = res.fullName;
+                  console.log(student.finalGrade);
+                  console.log(student.fullName);
+                },
+              });
+          });
+          console.log(res.data);
+        },
+        error: (err) => {
+          console.log("error", err);
+        },
+      });
   }
 
   navigateToTeachersClasses() {
@@ -108,27 +103,30 @@ export class ShowClassComponent {
 
   getFinalGrade(idUser: string) {
     this.idUser = idUser;
-    this.classroomService.getGrade(this.classId, this.idUser).subscribe({
-      next: (res: Grade) => {
-        this.studentGrade = res;
-        // this.finalGrade = this.studentGrade.finalGrade
-
-        this.graduationForm.patchValue({
-          promoted: this.studentGrade.finalGrade >= 6 ? true : false,
-          // promoted: this.finalGrade >= 6 ? true : false,
-        });
-        console.log("res", res);
-      },
-    });
+    this.classroomService
+      .getGrade(this.classId, this.idUser)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: (res: Grade) => {
+          this.studentGrade = res;
+          this.graduationForm.patchValue({
+            promoted: this.studentGrade.finalGrade >= 6 ? true : false,
+          });
+          console.log("res", res);
+        },
+      });
   }
 
   getClassroom() {
-    this.classroomService.getClassroom().subscribe({
-      next: (res) => {
-        this.classes = res;
-        console.log("prova", this.classes);
-      },
-    });
+    this.classroomService
+      .getClassroom()
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        next: (res) => {
+          this.classes = res;
+          console.log("prova", this.classes);
+        },
+      });
   }
 
   addGraduation() {
@@ -138,6 +136,7 @@ export class ShowClassComponent {
         this.classId,
         this.idUser
       )
+      .pipe(takeUntil(this.unsubscribe$))
       .subscribe({
         next: (res) => {
           console.log("tentativo", this.graduationForm.value);
@@ -149,6 +148,11 @@ export class ShowClassComponent {
         },
       });
     console.log(this.graduationForm.value);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next(true);
+    this.unsubscribe$.complete();
   }
 }
 
